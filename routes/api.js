b25f882852fd5eb0e05aeed033859c7bec020b20
Marvin Duran <marvinduran@beyondgames.co>
2016-02-08 21:46:16 -0600

    Movimiento lIbre Separation de Medicine de Azimuth reversa y directa

--- routes/api.js
+++ routes/api.js
@@ -12,13 +12,14 @@ var pinLectorPulsosAzimut = "A1";
 var pinLectorPulsosTilt = "A5";
 
 // Configuracion de Relacion =  Pulsos Por grado ( Pulsos/Grados)
-var relacionAzimut = 104/90;
-var relacionTilt = 65/21;
+var relacionAzimutI = 82/90;	//0.85
+var relacionAzimutD = 82/118; 	//0.72
+var relacionTilt = 44/30;
 
 // Configuracion de Sensores
-var frequenciaDeMuestreo = 1; 	// Milisegindo (Maxima muestra posible es de 10 ms)
+var frequenciaDeMuestreo = 1; 	// Milisegundos (Maxima muestra posible es de 10 ms)
 var thresholdDeCambio = 1; 		// 0-1023 - Jugar con esto si se detectan demasiados pulsos
-var voltajeMinimoDeAlto = 900; 	// 0 -1023 - Jugar con esto si se detectan demasiados o muy pocos pulsos
+var voltajeMinimoDeAlto = 40; 	// 0 -1023 - Jugar con esto si se detectan demasiados o muy pocos pulsos
 
 // Configuracion de Motores
 var velocidadMaxima = 255;		//1-255 - Representa 0 a 5 Volts en PWD
@@ -45,6 +46,7 @@ var tiltState = 	{ 	pulseCount : 0,
 						isActive : false
 					};			
 
+var movimientoLibre = false;
 var boardReady = false;
 
 //==========================================================
@@ -60,7 +62,7 @@ var createDigitalMotor = function (ledP, ledN) 	{  var motor = 	{ 	pinP 	: ledP,
 													return motor;
 												};
 var motorTilt, sensorTilt;
-var mototAzimut, sensorAzimut;
+var motorAzimut, sensorAzimut;
 
 //==========================================================
 //               ARDUINO COMMUNICATION
@@ -83,11 +85,11 @@ board.on("ready", function () {
 	console.log('Board ready');
 	boardReady = true;
 
-	mototAzimut = createDigitalMotor ( new five.Led(pinAzimutP), new five.Led(pinAzimutN) );
-	mototTilt = createDigitalMotor ( new five.Led(pinTiltP), new five.Led(pinTiltN) );
+	motorAzimut = createDigitalMotor ( new five.Led(pinAzimutP), new five.Led(pinAzimutN) );
+	motorTilt = createDigitalMotor ( new five.Led(pinTiltP), new five.Led(pinTiltN) );
 
-	mototAzimut.stop();
-	mototTilt.stop();
+	motorAzimut.stop();
+	motorTilt.stop();
 
 	sensorAzimut	= new five.Sensor({ pin: pinLectorPulsosAzimut, freq: frequenciaDeMuestreo, threshold: thresholdDeCambio });
 	sensorTilt		= new five.Sensor({ pin: pinLectorPulsosTilt, freq: frequenciaDeMuestreo, threshold: thresholdDeCambio });
@@ -95,47 +97,92 @@ board.on("ready", function () {
 	// sensorAzimut.within([ 1000, 1023 ], function() {
 	sensorAzimut.on("change", function() {
 		var currentV = this.value;
-		// if (currentV > voltajeMinimoDeAlto)
-		// 	{	
-				// console.log("Detected a Change voltage on Azimut: %s ", currentV);
-							
+		// console.log("Detected a Change voltage on Azimut: %s ", currentV);
+		if (currentV > voltajeMinimoDeAlto)
+			{	
 				if 		( currentV > azimutState.prevVoltage && !azimutState.isOn && azimutState.isActive)
 							{ 	
 								azimutState.isOn = true; 
-								// azimutState.prevVoltage = currentV; 
+								azimutState.prevVoltage = currentV; 
 							}
-				else if ( currentV < azimutState.prevVoltage && azimutState.isOn && azimutState.isActive)
-							{ 
-								azimutState.isOn = false; 	
-								azimutState.pulseCount++; 
-								console.log("azimut count up: %s with voltage %s ", azimutState.pulseCount, currentV);
-								// console.log("azimut count down: %s with voltage %s ", azimutState.pulseCount, currentV);
-							}
+				
+				
+			}
+		else if ( currentV < azimutState.prevVoltage && azimutState.isOn && azimutState.isActive)
+			{ 
+				azimutState.isOn = false; 	
+				azimutState.pulseCount++; 
 				azimutState.prevVoltage = currentV; 
-			// };
+				console.log("azimut count up: %s with voltage %s ", azimutState.pulseCount, currentV);
+				// console.log("azimut count down: %s with voltage %s ", azimutState.pulseCount, currentV);
+			};
 		
 	});
 
 	sensorTilt.on("change", function() {
 		var currentV = this.value;
+		// console.log("Detected a Change voltage on Tilt: %s ", currentV);
 		if (currentV > voltajeMinimoDeAlto)
 			{	
-				// console.log("Detected a Change voltage on Tilt: %s ", currentV);
 				if 		( currentV > tiltState.prevVoltage && !tiltState.isOn && tiltState.isActive)
 							{ 	
 								tiltState.isOn = true; 
+								tiltState.prevVoltage = currentV; 
 								// console.log("azimut count down: %s with voltage %s ", azimutState.pulseCount, currentV);
 							}
-				else if ( currentV < tiltState.prevVoltage && tiltState.isOn && tiltState.isActive)
-							{ 
-								tiltState.isOn = false; 	
-								tiltState.pulseCount++ 
-								console.log("azimut count up: %s with voltage %s ", azimutState.pulseCount, currentV); 
-							}
+				
+			}
+		else if ( currentV < tiltState.prevVoltage && tiltState.isOn && tiltState.isActive)
+			{ 
+				tiltState.isOn = false; 	
+				tiltState.pulseCount++ 
+				tiltState.prevVoltage = currentV; 
+				console.log("tilt count up: %s with voltage %s ", tiltState.pulseCount, currentV); 
 			};
-			tiltState.prevVoltage = currentV; 
 		
 	});
+});
+
+router.post("/moverAziF", function (request, response) {
+    log(request); 
+	response.json({msg: "Movimiento Libre Azimut en Directa"});
+    if(boardReady && !azimutState.isActive && !tiltState.isActive){
+    	azimutState.isActive = true;
+    	motorAzimut.forward();
+   	};
+});
+router.post("/moverAziR", function (request, response) {
+    log(request); 
+	response.json({msg: "Movimiento Libre Azimut en Inversa"});
+    if(boardReady && !azimutState.isActive && !tiltState.isActive){
+    	azimutState.isActive = true;
+    	motorAzimut.reverse();
+   	};
+});
+
+router.post("/moverTiF", function (request, response) {
+    log(request); 
+	response.json({msg: "Movimiento Libre Tilt en Directa"});
+    if(boardReady && !azimutState.isActive && !tiltState.isActive){
+    	tiltState.isActive = true;
+    	motorTilt.forward();
+   	};
+});
+router.post("/moverTiR", function (request, response) {
+    log(request); 
+	response.json({msg: "Movimiento Libre Tilt en Inversa"});
+    if(boardReady && !azimutState.isActive && !tiltState.isActive){
+    	tiltState.isActive = true;
+    	motorTilt.reverse();
+   	};
+});
+router.post("/parar", function (request, response) {
+    log(request); 
+	response.json({msg: "Parando movimiento"});
+	tiltState.isActive = false;
+	azimutState.isActive = false;
+	motorAzimut.stop();
+	motorTilt.stop();
 });
 
 
@@ -149,15 +196,16 @@ router.post("/moverA", function (request, response) {
     	var gradosAzimut = request.body.azimut;  // Valor en gradosAzimut
 
 		gradosAzimut -= azimutState.grades;
+		if (gradosAzimut > 0) {relacionAzimut = relacionAzimutD} else {relacionAzimut = relacionAzimutI}
 		var movimientoEnPulsosAzimut = Math.abs(Math.round(gradosAzimut * relacionAzimut));
-		if(gradosAzimut > 0)  	{ mototAzimut.forward(velocidadMinima); } 
-		else 					{ mototAzimut.reverse(velocidadMaxima); };
+		if(gradosAzimut > 0)  	{ motorAzimut.forward(velocidadMinima); } 
+		else 					{ motorAzimut.reverse(velocidadMaxima); };
 		// console.log("movimiento azimut empezando hacia " + request.body.azimut + ", Ultimo " + lastAzimutMovement);
 
 		var timerAzimut = setInterval(function(){
 			if(movimientoEnPulsosAzimut <= azimutState.pulseCount){
 				console.log("movimiento azimut terminado, count " + movimientoEnPulsosAzimut);
-				mototAzimut.stop();
+				motorAzimut.stop();
 				clearInterval(timerAzimut);
 				azimutState.pulseCount = 0;
     	
@@ -172,14 +220,14 @@ router.post("/moverA", function (request, response) {
 
 		gradosTilt -= tiltState.grades;
 		var movimientoEnPulsosTilt = Math.abs(Math.round(gradosTilt * relacionTilt));
-		if(gradosTilt > 0)  	{ mototTilt.forward(velocidadMinima); } 
-		else 					{ mototTilt.reverse(velocidadMaxima); };
+		if(gradosTilt > 0)  	{ motorTilt.forward(velocidadMinima); } 
+		else 					{ motorTilt.reverse(velocidadMaxima); };
 		// console.log("movimiento azimut empezando hacia " + request.body.azimut + ", Ultimo " + lastAzimutMovement);
 
 		var timerTilt = setInterval(function(){
 			if(movimientoEnPulsosTilt <= tiltState.pulseCount){
 				console.log("movimiento tilt terminado, count " + movimientoEnPulsosTilt);
-				mototTilt.stop();
+				motorTilt.stop();
 				clearInterval(timerTilt);
 				tiltState.pulseCount = 0;
     	
